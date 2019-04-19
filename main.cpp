aed2e1faa2748d64b4a02ced4e379a8033394146
Thalya Maerten <seris@protonmail.ch>
2019-04-19 16:38:59 -0400

    add a small framework to change altitude

--- main.cpp
+++ main.cpp
@@ -2,69 +2,69 @@
 
 int main(int argc, char* argv[])
 {
+	if(argc < 2 || string(argv[1]) != "-v" ) {
+		cout.setstate(ios::failbit);
+	}
+	
 	ofstream plane_altitude("./plane_altitude", ios::out | ios::trunc);
 	ofstream plane_speed("./plane_speed", ios::out | ios::trunc);
 	ofstream plane_angle("./plane_angle", ios::out | ios::trunc);
 
 	auto timestep = 100us;
 	// every minute in simulation time
-	auto tick_count_before_dump = 10 * 1000 + 27; 
+	auto tick_count_before_dump = 10 * 1000 + 1; 
 
-	Plane plane(timestep);
-	plane.set_altitude_transfer(500);
+	Plane plane(timestep, 10);
 
 	cout << "plane initial status:" << endl;
 	plane.dump_status();
 	cout << endl;
 
-	if(argc < 2 || string(argv[1]) != "-v" ) {
-		cout.setstate(ios::failbit);
-	}
+	static auto wait_until = 0;
 
 	uint64_t tick_count = 0;
 	while(true) {	
 		tick_count++;
 		plane.tick();
 		
+		auto time_elapsed = duration_cast<seconds>(timestep * tick_count).count();
+
 		// every minute
 		if(tick_count % tick_count_before_dump == 0) {
-			auto time_elapsed = duration_cast<seconds>(timestep * tick_count).count();
 			auto plane_data = plane.get_data();
+			auto x = plane_data.pos_x_ft;
 
-			plane_altitude << time_elapsed << '\t' << plane_data.altitude_ft << "ft" << endl;
-			plane_speed << time_elapsed << '\t' << plane_data.speed_m_min << "m/min" << endl;
-			plane_angle << time_elapsed << '\t' << plane_data.computed_angle_deg << "deg" << endl;
+			plane_altitude << x << '\t' << plane_data.altitude_ft << "ft" << endl;
+			plane_speed << x << '\t' << plane_data.vertical_speed << "m/min" << endl;
+			plane_angle << x << '\t' << plane_data.computed_angle_deg << "deg" << endl;
 
 			cout << '[' << time_elapsed << "s]" << endl;
 			plane.dump_status();
+			cout << endl;	
+			// usleep(duration_cast<microseconds>(50ms).count());	
+		}
+	
+		if((wait_until == 0 && plane.is_altitude_transfer_done()) || (wait_until != 0 && time_elapsed > wait_until)) {
+			plane.dump_status();
 			cout << endl;
 
-			if(time_elapsed == 150){
-				plane.set_altitude_transfer(0);
-			}
+			wait_until = 0;
 
-			if(time_elapsed == 200) {
-				plane.set_altitude_transfer(300);
-			}
+			int new_altitude = 0;
+			cout << "new altitude: ";
+			cin >> new_altitude;
+			cin.ignore();
 
-			if(time_elapsed == 224) {
-				plane.set_altitude_transfer(700);
+			if(new_altitude >= 0) {
+				plane.set_altitude_transfer(new_altitude);
+				cout << "going to altitude " << new_altitude << endl;
+			} else {
+				auto waiting_time = -new_altitude;
+				wait_until = time_elapsed + waiting_time;	
+				cout << "waiting for " << waiting_time << "s" << endl;
 			}
-
-			if(time_elapsed == 400) {
-				plane.set_altitude_transfer(0);
-			}
-
-			if(time_elapsed == 500) {
-				plane.set_altitude_transfer(15, 5000, 40000);
-			}
-			
-			if(time_elapsed == 600) {
-				return EXIT_SUCCESS;
-			}
-
-//			usleep(duration_cast<microseconds>(50ms).count());
 		}
+
 
 		try {
 			assert_constraints(plane);	
